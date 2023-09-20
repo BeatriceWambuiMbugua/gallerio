1a1879ad1d9ef8d731e8a170cdd51a3faa114ea0
BeatriceWambuiMbugua <wambuimbeatrice@gmail.com>
2023-09-20 21:23:49 +0300

    Refactor `Home` component to add suspense fallback for login and gallery components.

--- app/page.js
+++ app/page.js
@@ -1,19 +1,22 @@
+"use client";
 
-'use client';
-
-import Gallery from '@/components/Gallery';
-import LoginPage from '@/components/Login';
-import { useSession } from 'next-auth/react';
+import Gallery from "@/components/Gallery";
+import LoginPage from "@/components/Login";
+import { useSession } from "next-auth/react";
+import { Suspense } from "react";
+import Loading from "./loading";
 
 export default function Home() {
   const { data: session, status } = useSession();
-  
 
   // If the user is not logged in, show a login link
   if (!session) {
     return (
       <div>
-        <LoginPage/>
+        <Suspense fallback={<Loading />}>
+        <LoginPage />
+        </Suspense>
+        
       </div>
     );
   }
@@ -21,8 +24,9 @@ export default function Home() {
   // If the user is logged in, display the image gallery
   return (
     <div>
-      <Gallery/>
+      <Suspense fallback={<Loading />}>
+        <Gallery />
+      </Suspense>
     </div>
   );
 }
-
