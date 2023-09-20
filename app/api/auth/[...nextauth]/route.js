15245a9c24d4865c58ed522b63b2178e4adfb977
BeatriceWambuiMbugua <wambuimbeatrice@gmail.com>
2023-09-20 13:20:06 +0300

    [object Promise] Add CredentialsProvider to handle authentication in NextAuth route.js. This allows users to sign in using email and password credentials.

---
+++ app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,38 @@
+import NextAuth from "next-auth/next";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+const handler = NextAuth({
+  providers: [
+    CredentialsProvider({
+      // The name to display on the sign in form (e.g. "Sign in with...")
+      name: "Credentials",
+      // `credentials` is used to generate a form on the sign in page.
+      // You can specify which fields should be submitted, by adding keys to the `credentials` object.
+      // e.g. domain, username, password, 2FA token, etc.
+      // You can pass any HTML attribute to the <input> tag through the object.
+      credentials: {
+        email: {
+          label: "Email",
+          type: "email",
+          placeholder: "Enter your Email",
+        }, // You can customize this label to 'Email'
+        password: { label: "Password", type: "password" }, // You can customize this label to 'Password'
+      },
+      authorize: async (credentials) => {
+        // Add your own authentication logic here
+        if (
+          credentials.email === "user@example.com" &&
+          credentials.password === "1Password"
+        ) {
+          // If credentials are valid, return the user object
+          return Promise.resolve({ email: credentials.email });
+        } else {
+          // If credentials are invalid, return null
+          return Promise.resolve(null);
+        }
+      },
+    }),
+  ],
+});
+
+export { handler as GET, handler as POST };
