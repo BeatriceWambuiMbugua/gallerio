391c8b86a06c13e486538b14ddb2d46063440a63
BeatriceWambuiMbugua <wambuimbeatrice@gmail.com>
2023-09-20 20:15:12 +0300

    Remove search functionality from Navbar component.

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,7 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
-import { BsHash } from "react-icons/bs";
+
 
 export default function Navbar() {
   return (
@@ -20,44 +19,7 @@ export default function Navbar() {
           </p>
         </Link>
 
-        <form className="md:flex items-center hidden">
-          <label htmlFor="simple-search" className="sr-only">
-            Search
-          </label>
-          <div className="relative w-full lg:w-[500px] ">
-            <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-              <BsHash className="w-4 h-4 text-gray-400" />
-            </div>
-            <input
-              type="text"
-              id="simple-search"
-              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg  focus:outline-none focus:ring-gray-500 focus:border-gray-600 block w-full pl-10 p-2.5"
-              placeholder="Search Image..."
-              required
-            />
-          </div>
-          <button
-            type="submit"
-            className="p-2.5 ml-2 text-sm font-medium text-slate-50 bg-blue-700 rounded-lg border border-blue-700 hover:bg-blue-800 "
-          >
-            <svg
-              className="w-4 h-4"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 20 20"
-            >
-              <path
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
-              />
-            </svg>
-            <span className="sr-only">Search</span>
-          </button>
-        </form>
+       
         <Link href={"/"}>
           <button
             type="submit"
