a9a1227457e5453bc296688fc1245c3416c21c2e
BeatriceWambuiMbugua <wambuimbeatrice@gmail.com>
2023-09-20 17:09:53 +0300

    [Navbar] fix class attribute in form and buttons, improve accessibility

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -20,28 +20,28 @@ export default function Navbar() {
           </p>
         </Link>
 
-        <form class="md:flex items-center hidden">
-          <label for="simple-search" class="sr-only">
+        <form className="md:flex items-center hidden">
+          <label htmlFor="simple-search" className="sr-only">
             Search
           </label>
-          <div class="relative w-full lg:w-[500px] ">
-            <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+          <div className="relative w-full lg:w-[500px] ">
+            <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
               <BsHash className="w-4 h-4 text-gray-400" />
             </div>
             <input
               type="text"
               id="simple-search"
-              class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg  focus:outline-none focus:ring-gray-500 focus:border-gray-600 block w-full pl-10 p-2.5"
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg  focus:outline-none focus:ring-gray-500 focus:border-gray-600 block w-full pl-10 p-2.5"
               placeholder="Search Image..."
               required
             />
           </div>
           <button
             type="submit"
-            class="p-2.5 ml-2 text-sm font-medium text-slate-50 bg-blue-700 rounded-lg border border-blue-700 hover:bg-blue-800 "
+            className="p-2.5 ml-2 text-sm font-medium text-slate-50 bg-blue-700 rounded-lg border border-blue-700 hover:bg-blue-800 "
           >
             <svg
-              class="w-4 h-4"
+              className="w-4 h-4"
               aria-hidden="true"
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
@@ -49,19 +49,19 @@ export default function Navbar() {
             >
               <path
                 stroke="currentColor"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
                 d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
               />
             </svg>
-            <span class="sr-only">Search</span>
+            <span className="sr-only">Search</span>
           </button>
         </form>
         <Link href={"/"}>
           <button
             type="submit"
-            class="text-neutral-900 bg-gray-200 hover:bg-gray-100 focus:ring-1 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm sm:w-auto px-5 py-2.5 text-center hover:subpixel-antialiased "
+            className="text-neutral-900 bg-gray-200 hover:bg-gray-100 focus:ring-1 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm sm:w-auto px-5 py-2.5 text-center hover:subpixel-antialiased "
           >
             Login
           </button>
