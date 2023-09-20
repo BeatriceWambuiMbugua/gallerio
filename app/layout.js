c2f76d36d97ac808e0940510593593e142caa83b
BeatriceWambuiMbugua <wambuimbeatrice@gmail.com>
2023-09-20 13:21:12 +0300

    [Navbar] Add navbar component to layout for better navigation.

--- app/layout.js
+++ app/layout.js
@@ -1,17 +1,21 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
+import Navbar from "@/components/Navbar";
+import "./globals.css";
+import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'Gallerio',
-  description: 'Visual World Exploration',
-}
+  title: "Gallerio",
+  description: "Visual World Exploration",
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Navbar />
+        {children}
+      </body>
     </html>
-  )
+  );
 }
