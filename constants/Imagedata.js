9c759439d0c0077fd9ad65ec195234ccbd273fea
BeatriceWambuiMbugua <wambuimbeatrice@gmail.com>
2023-09-20 16:44:24 +0300

    [constants] Add Imagedata.js file

---
+++ constants/Imagedata.js
@@ -0,0 +1,51 @@
+const imageData = [
+  {
+    id: 1,
+    url: "https://img.freepik.com/free-photo/horizontal-shot-pretty-dark-skinned-woman-with-afro-hairstyle-has-broad-smile-white-teeth-shows-something-nice-friend-points-upper-right-corner-stands-against-wall_273609-16442.jpg?w=1480&t=st=1695206461~exp=1695207061~hmac=bdeed05553ece8a0a8b508215b319c5768a712e203ab8afbc769ea5a6199d3aa",
+    tags: ["woman", "people"],
+  },
+  {
+    id: 2,
+    url: "https://img.freepik.com/free-photo/glad-lovely-african-american-female-asks-keep-silence-holds-index-finger-lips-has-cheerful-expression-dressed-casually-poses-against-white-wall-people-secrecy-ethnicity-concept_273609-16447.jpg?w=1480&t=st=1695206512~exp=1695207112~hmac=77cf4ef9d418e17b9c40138e6b46fdff3264b4333a32fb9a4702e241b290ca44",
+    tags: ["woman", "people"],
+  },
+  {
+    id: 3,
+    url: "https://img.freepik.com/free-photo/good-deal-right-corner-confident-pleasant-friendly-looking-african-american-gorgeous-woman-with-afro-haircut-asking-check-out-visit-store-page-pointing-finger-left-smiling-looking-camera_1258-85037.jpg?w=1480&t=st=1695206541~exp=1695207141~hmac=3528784e908ed288635c8703e0774b06edad3f9d67b3c9bbd8527f48d54ce116",
+    tags: ["people", "woman"],
+  },
+  {
+    id: 4,
+    url: "https://img.freepik.com/free-photo/closeup-shot-two-zebras-cuddling_181624-5940.jpg?w=1480&t=st=1695206618~exp=1695207218~hmac=909c6a166d0d3535735fddcbc3d248c9efa1647e03bddc2e156d1591e38db0c6",
+    tags: ["animals", "zebra"],
+  },
+  {
+    id: 5,
+    url: "https://img.freepik.com/free-photo/closeup-shot-elephants-standing-near-lake-sunset_181624-29375.jpg?w=1480&t=st=1695206658~exp=1695207258~hmac=b6dc0a1c16a329a2726516ba03d2ae3642dd64d66fd4d7b970dbf94011559451",
+    tags: ["animals", "elephant"],
+  },
+  {
+    id: 6,
+    url: "https://img.freepik.com/free-photo/african-lion-portrait-warm-light_475641-46.jpg?w=1480&t=st=1695206702~exp=1695207302~hmac=1a521ac3bf75d9249d188a2199111f822b8285bf0af6389db955f08f115ac694",
+    tags: ["animals", "lion"],
+  },
+  {
+    id: 7,
+    url: "https://img.freepik.com/free-photo/amazing-bengal-tiger-nature_475641-1137.jpg?w=1480&t=st=1695207631~exp=1695208231~hmac=f07d04231cd1fe3ac4f4a95b46d3f6369972886c310a169c830e5b63f52dd2e7",
+    tags: ["animals", "leopard"],
+  },
+  {
+    id: 8,
+    url: "https://img.freepik.com/free-photo/blue-jeep-parking-public-zone_114579-4042.jpg?w=1480&t=st=1695206811~exp=1695207411~hmac=04ec1c954d04e86b0174688c5a8059aacdb977dad33a560dadfdb1daef5c29c2",
+    tags: ["vehicle", "car"],
+  },
+  {
+    id: 9,
+    url: "https://img.freepik.com/free-photo/black-sport-car-highway-drive-sunset_114579-5064.jpg?w=1480&t=st=1695206875~exp=1695207475~hmac=c9a405fb33a7828bf4184ee2b1fe66729d9dc420fe6010a197a40ac87a156384",
+    tags: ["car", "sunrise"],
+  },
+
+  // Add more image objects as needed
+];
+
+export default imageData;
