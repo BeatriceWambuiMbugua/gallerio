5e6bbc47b11d871635eec6a3889abdce0329eabb
BeatriceWambuiMbugua <wambuimbeatrice@gmail.com>
2023-09-21 05:35:16 +0300

    Change Navbar component to display "Logout" button instead of "Login" button when user is logged in, and vice versa.

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,8 +1,10 @@
+"use client"
 import Image from "next/image";
 import Link from "next/link";
-
+import { useSession, signOut } from "next-auth/react";
 
 export default function Navbar() {
+  const { data: session } = useSession();
   return (
     <div>
       <div className="container mx-auto flex items-center justify-between  pt-4 px-2 md:px-0">
@@ -19,15 +21,23 @@ export default function Navbar() {
           </p>
         </Link>
 
-       
-        <Link href={"/"}>
+        {session ? (
           <button
-            type="submit"
+            onClick={() => signOut()}
             className="text-neutral-900 bg-gray-200 hover:bg-gray-100 focus:ring-1 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm sm:w-auto px-5 py-2.5 text-center hover:subpixel-antialiased "
           >
-            Login
+            Logout
           </button>
-        </Link>
+        ) : (
+          <Link href={"/"}>
+            <button
+              type="submit"
+              className="text-neutral-900 bg-gray-200 hover:bg-gray-100 focus:ring-1 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm sm:w-auto px-5 py-2.5 text-center hover:subpixel-antialiased "
+            >
+              Login
+            </button>
+          </Link>
+        )}
       </div>
     </div>
   );
